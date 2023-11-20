99ccb1f20e304cce8e77d75cd462d70e7537d28b
fariviriski <7634@holbertonstudents.com>
2023-11-20 12:25:30 -0400

    brr

--- _printf.c
+++ _printf.c
@@ -22,12 +22,7 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 		len = 0;
 		if (*(format + i) == '%')
 		{
-			flag = handle_flags(format + i + 1);
-			tmp = count_one_bits(flag);
-			wid = handle_width(args, format + i + tmp + 1, &tmp);
-			prec = handle_precision(args, format + i + tmp + 1, &tmp);
-			len = handle_length(format + i + tmp + 1);
-			tmp += (len != 0) ? 1 : 0;
+
 			f = handle_specifiers(format + i + tmp + 1);
 			if (f != NULL)
 			{
