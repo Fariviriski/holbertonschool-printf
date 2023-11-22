73e126f8985d7f7478fea9a2001e312d597f1a6f
madja0125 <dejesusmiguel788@gmail.com>
2023-11-22 04:40:26 +0000

    adding a more simple version of a custom prints let me know what you think

---
+++ printf_helper.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+
+/**
+ * print_char - function that prints a character
+ * @ls: list with elements
+ * @counter: sum of character printed
+ * Return: amount of charcter printed in function
+ */
+int print_char(va_list ls, int *counter)
+{
+	char c;
+
+	c = va_arg(ls, int);
+
+
+	_putchar(c);
+	(*counter)++;
+	return (1);
+}
+
+/**
+ * print_string - function that print a string
+ * @ls: list with elements
+ * @counter: sum of character printed
+ * Return: str with the total of character printed in function
+ */
+int print_string(va_list ls, int *counter)
+{
+	const char *s;
+	int str = 0;
+
+	s = va_arg(ls, const char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		(*counter)++;
+		str++;
+		s++;
+	}
+	return (str);
+}
+
+/**
+ * print_percent - function that prints a percent sign
+ * @counter: the position in the string
+ * Return: character printed
+ */
+int print_percent(int *counter)
+{
+	char p = '%';
+
+	_putchar(p);
+	(*counter)++;
+	return (1);
+}
